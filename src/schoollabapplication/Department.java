44ec41bdbb5b1f67a37f72d63e8219d09895b872
HAGOS <hagos.bezabh@gmail.com>
2016-02-05 19:31:53 -0900

    update

--- src/schoollabapplication/Department.java
+++ src/schoollabapplication/Department.java
@@ -70,6 +70,37 @@ public class Department {
 
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((people == null) ? 0 : people.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (people == null) {
+			if (other.people != null)
+				return false;
+		} else if (!people.equals(other.people))
+			return false;
+		return true;
+	}
+
 	public void addPerson(Person obj) {
 		people.add(obj);
 	}
